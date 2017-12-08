9a14ad75858987b6663ff0af5ea945c41d0c6a62
Nicolai Parlog <nipa@codefx.org>
2017-12-08 10:31:11 +0100

    Turn null parent class loader test into integration test
    The test relies on the presence of the project JAR, so it needs to run
    after the `package` phase.

---
+++ null-parent-classloader/src/test/java/NullParentClassLoaderIT.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class NullParentClassLoaderIT {
+
+	/**
+	 * Attempt to load a class from the URLClassLoader that references a java.sql.* class. The
+	 * java.sql.* package is one of those not visible to the Java 9 bootstrap class loader that
+	 * was visible to the Java 8 bootstrap class loader.
+	 */
+	@Test
+	public void loadSqlDateUsingNullParent(TestReporter reporter) throws Exception {
+		URLClassLoader loader = buildLoader(reporter);
+
+		Class<?> jsqlUserClass = loader.loadClass("JavaSqlUser");
+		reporter.publishEntry("Loaded class", jsqlUserClass.toString());
+
+		Object jsqlUser = jsqlUserClass.getConstructor().newInstance();
+		reporter.publishEntry("Created instance", jsqlUser.toString());
+
+		loader.close();
+	}
+
+	/**
+	 * Build a URLClassLoader that only includes the null-parent-classloader artifact in its classpath,
+	 * along with the bootstrap class loader as its parent.
+	 *
+	 * @throws MalformedURLException
+	 * 		on failure to build the classpath URL
+	 */
+	private URLClassLoader buildLoader(TestReporter reporter) throws MalformedURLException {
+		String testRootFolder = NullParentClassLoaderIT.class.getResource("/").getPath();
+		reporter.publishEntry("Test classes folder", testRootFolder);
+		Path projectJar = Paths
+				.get(testRootFolder)
+				.getParent()
+				.resolve("null-parent-classloader-1.0-SNAPSHOT.jar");
+
+		assumeTrue(
+				projectJar.toFile().exists(),
+				"Project JAR must exist as " + projectJar.toString() + " for test to be executed.");
+		reporter.publishEntry("Project JAR", projectJar.toString());
+
+		URL path[] = { projectJar.toUri().toURL() };
+		// this is the parent that is required when running under Java 9:
+		// ClassLoader parent = ClassLoader.getPlatformClassLoader();
+		ClassLoader parent = null;
+		URLClassLoader loader = new URLClassLoader(path, parent);
+		reporter.publishEntry("Class loader", loader.toString());
+
+		return loader;
+	}
+
+}
+
