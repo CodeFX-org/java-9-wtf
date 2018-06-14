021cb358f4f0bc4ca36e8a0a5f60618df27f800a
Nicolai Parlog <nipa@codefx.org>
2018-06-14 15:55:15 +0200

    Update Maven-JAXB2-Plugin to include version that works on Java 9

--- maven-jaxb2-plugin/src/test/java/wtf/java9/maven_jaxb2_plugin/JaxbPluginTest.java
+++ maven-jaxb2-plugin/src/test/java/wtf/java9/maven_jaxb2_plugin/JaxbPluginTest.java
@@ -6,19 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JaxbPluginTest {
 
-	@Test // extected to pass on Java 9
+	@Test // expected to pass on Java 9
 	public void simpleCaseWorks() throws Exception {
 		Object simpleClass = Class.forName("wtf.java9.maven_jaxb2_plugin.SimpleClass").newInstance();
 		assertThat(simpleClass).isNotNull();
 	}
 
-	@Test // extected to fail on Java 9 because the binding is not processed
+	@Test // expected to fail on Java 9 because the binding is not processed
 	public void packageBindingRespected() throws Exception {
 		Object packageBinding = Class.forName("wtf.java9.maven_jaxb2_plugin.with.pack.SimpleClassWithPackage").newInstance();
 		assertThat(packageBinding).isNotNull();
 	}
 
-	@Test // extected to fail on Java 9 because the binding is not processed
+	@Test // expected to fail on Java 9 because the binding is not processed
 	public void classNameBindingRespected() throws Exception {
 		Object classNameBinding = Class.forName("wtf.java9.maven_jaxb2_plugin.SimpleClassWithRightName").newInstance();
 		assertThat(classNameBinding).isNotNull();
